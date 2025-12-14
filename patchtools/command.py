170d177b2af458fb4c10f0608337486486614dbc
Lee Duncan <lduncan@suse.com>
2025-12-14 12:40:33 -0800

    fix error in run_command

--- patchtools/command.py
+++ patchtools/command.py
@@ -8,6 +8,6 @@ def run_command(command, stdin=None, our_input=None, stdout=subprocess.PIPE):
     with open("/dev/null", "w") as dn:
         proc = subprocess.run(command, shell=True, encoding='utf-8',
                               check=False,
-                              stdin=stdin, input=our_nput, stdout=stdout,
+                              stdin=stdin, input=our_input, stdout=stdout,
                               stderr=dn)
         return proc.stdout
