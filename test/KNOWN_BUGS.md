857853a3eadf9f24f0febb29e35d2ed143ac1178
Lee Duncan <lduncan@suse.com>
2025-12-19 14:34:02 -0800

    Testing: add a test directory and test files
    Just a start, but first test should pass.

--- test/KNOWN_BUGS.md
+++ test/KNOWN_BUGS.md
@@ -1,5 +1,19 @@
 # Bugs that are known
 
-* exporting a commit with UTF-8 seems to get strange chars
+## exporting a commit with a UTF-8 string seems to get strange chars
 
 check out this commit: 8db816c6f176321e42254badd5c1a8df8bfcfdb4
+
+This may or may not be a bug? It leaves the "from" name kind
+of funky, e.g.:
+
+    zsh> exportpatch  8db816c6f176321e42254badd5c1a8df8bfcfdb4 | head -1
+    From: =?utf-8?q?Kai_M=C3=A4kisara_=3CKai=2EMakisara=40kolumbus=2Efi=3E?=
+
+but ''git show'' on the same commit has different output:
+
+    zsh git show 8db816c6f176321e42254badd5c1a8df8bfcfdb4 | head -2
+    commit 8db816c6f176321e42254badd5c1a8df8bfcfdb4
+    Author: Kai Mäkisara <Kai.Makisara@kolumbus.fi>
+
+So should be rendering utf charaters better?
---
+++ test/test_exportpatch.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3.11
+"""The test suite for the patchtools exportpatch command."""
+
+import filecmp
+import tempfile
+import unittest
+from pathlib import Path
+from subprocess import PIPE, run
+
+
+def my_run_command(command, stdin=None, our_input=None, stdout=PIPE, cwd=None):
+    """Run the specified command."""
+    with Path('/dev/null').open('wb') as dn:
+        proc = run(command.split(), encoding='utf-8', check=False, cwd=cwd,
+                   stdin=stdin, input=our_input, stdout=stdout, stderr=dn)
+        return (proc.returncode, proc.stdout)
+
+
+def get_patch_path(fname, dirname=None, prefix='', suffix='', truncate=64):
+    """Return a patch filename that isn't too long"""
+    print(f'\nDEBUG: input fname={fname}, dirname={dirname}, suffix={suffix} ...')
+    truncate_chars = truncate - len(fname) - len(prefix + suffix)
+    if truncate_chars < 0:
+        print(f'DEBUG: truncating string by {truncate_chars} characters')
+        fname = fname[0:truncate_chars]
+    fpath = Path(prefix + fname + suffix)
+    if dirname:
+        fpath = Path(dirname) / fpath
+    print(f'DEBUG: returning fpath={fpath}')
+    return fpath
+
+
+# the command we are here to test
+test_cmd = 'exportpatch'
+
+# a known commit we use for simple tests
+known_st_commit = '8db816c6f176321e42254badd5c1a8df8bfcfdb4'
+
+# the base patch name for our known st commit (no suffix, etc)
+known_st_patch = 'scsi-st-Tighten-the-page-format-heuristics-with-MODE-SELECT'
+
+
+class TestExportpatchNormalFunctionality(unittest.TestCase):
+
+    def test_output_to_file(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            # make sure our output file doesn't exist (even though its a temp dir)
+            ofile_path = get_patch_path(known_st_patch, dirname=tmpdir)
+            ofile_path.unlink(missing_ok=True)
+            # run the command, saving output to a file in our tmpdir
+            (res, pname) = my_run_command(f'{test_cmd} -w -d {tmpdir} {known_st_commit}')
+            # ensure command succeeded
+            self.assertEqual(res, 0, f'running {test_cmd} failed')
+            # ensure name returned is the same
+            self.assertEqual(pname.strip(), ofile_path.name, 'patch name wrong')
+            # compare the commit file with a known good one
+            res = filecmp.cmp(ofile_path, f'test/data/{known_st_patch}.known_good')
+            self.assertEqual(res, True, 'patch file differs from known good')
+
+            # test same thing but with suffix
+            ofile_path = get_patch_path(known_st_patch, dirname=tmpdir, suffix='.patch')
+            ofile_path.unlink(missing_ok=True)
+            # run the command, saving output to a file in our tmpdir
+            (res, pname) = my_run_command(f'{test_cmd} -w -s -d {tmpdir} {known_st_commit}')
+            # ensure command succeeded
+            self.assertEqual(res, 0, f'running {test_cmd} failed')
+            # ensure name returned is the same
+            self.assertEqual(pname.strip(), ofile_path.name, 'patch name wrong')
+            # compare the commit file with a known good one
+            res = filecmp.cmp(ofile_path, f'test/data/{known_st_patch}.known_good')
+            self.assertEqual(res, True, 'patch file differs from known good')
+
+
+if __name__ == '__main__':
+    unittest.main()
